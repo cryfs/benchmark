503b96f7169ff5aac9afac3fb3c9088707743c3c
Sebastian Messmer <mail@smessmer.de>
2015-09-24 21:37:21 +0200

    Improved csv output

--- benchmark/bonnie.py
+++ benchmark/bonnie.py
@@ -8,18 +8,20 @@ class Bonnie(object):
 
     def run(self, numRuns):
         output = subprocess.check_output(
-            #["bonnie++", "-d", self.dir, "-m", self.name, "-x", str(numRuns), "-r", "128", "-s", "256"])
-            ["bonnie++", "-d", self.dir, "-m", self.name, "-x", str(numRuns)])
-        return self._generate_csv_from_output(output)
+            ["bonnie++", "-d", self.dir, "-m", self.name, "-x", str(numRuns), "-r", "128", "-s", "256", "-n", "256"])
+            #["bonnie++", "-d", self.dir, "-m", self.name, "-x", str(numRuns), "-n", "256"])
+        return self._generate_csv_from_output(output, numRuns)
 
-    def _generate_csv_from_output(self, output):
-        header_line = ("name,file_size,putc,putc_cpu,put_block,put_block_cpu,rewrite,rewrite_cpu,getc,getc_cpu,"
-                       "get_block,get_block_cpu,seeks,seeks_cpu,num_files,seq_create,seq_create_cpu,seq_stat,"
-                       "seq_stat_cpu,seq_del,seq_del_cpu,ran_create,ran_create_cpu,ran_stat,ran_stat_cpu,ran_del,"
-                       "ran_del_cpu")
+    def header_line(self):
+        return ("name,file_size,putc,putc_cpu,put_block,put_block_cpu,rewrite,rewrite_cpu,getc,getc_cpu,get_block,"
+                "get_block_cpu,seeks,seeks_cpu,num_files,seq_create,seq_create_cpu,seq_stat,seq_stat_cpu,seq_del,"
+                "seq_del_cpu,ran_create,ran_create_cpu,ran_stat,ran_stat_cpu,ran_del,ran_del_cpu")
+
+    def _generate_csv_from_output(self, output, expectedNumLines):
         lines = output.decode().strip().splitlines()
-        if len(lines) == 1:
-            return [header_line] + lines
-        else:
-            assert (lines[0] == header_line)
-            return lines
+        if lines[0] == self.header_line():
+            lines = lines[1:]
+        expected_num_column_separators = self.header_line().count(",")
+        assert(all([line.count(",") == expected_num_column_separators for line in lines]))
+        assert(len(lines) == expectedNumLines)
+        return "\n".join(lines)
