775266b167741071194e7c287f196aa6b655896a
Sebastian Messmer <mail@smessmer.de>
2015-09-24 21:26:05 +0200

    Honor runs argument

--- benchmark/main.py
+++ benchmark/main.py
@@ -5,20 +5,20 @@ from benchmark.filesystems import PlainFs, EncFs
 
 def main():
     parser = argparse.ArgumentParser(description="Run filesystem benchmarks comparing cryfs to other file systems")
-    parser.add_argument('--dir', required=True)
-    parser.add_argument('--runs', type=int, default=1)
+    parser.add_argument('--dir', required=True, help="Directory where the benchmark is run.")
+    parser.add_argument('--runs', type=int, default=1, help="Number of runs.")
     args = parser.parse_args()
-    run_benchmark(args.dir)
+    run_benchmark(args.dir, args.runs)
 
 
-def run_benchmark(dir):
+def run_benchmark(dir, numRuns):
     # Setup only needed for filebench
     #setup.setup()
 
     with PlainFs(dir) as fs:
-        plain = Bonnie(name="plain", dir=fs.mount_dir).run(2)
+        plain = Bonnie(name="plain", dir=fs.mount_dir).run(numRuns)
 
     with EncFs(dir) as fs:
-        encfs = Bonnie(name="EncFS", dir=fs.mount_dir).run(2)
+        encfs = Bonnie(name="EncFS", dir=fs.mount_dir).run(numRuns)
 
     print("Output plain:\n%s\n\nOutput EncFs:\n%s\n" % (plain, encfs))
