5e5c3b7cd6628e919230adbb33c5113acf76e50e
Sebastian Messmer <mail@smessmer.de>
2015-10-07 04:06:08 +0200

    Removed custom test size, since we set it to the default anyhow

--- benchmark/main.py
+++ benchmark/main.py
@@ -16,38 +16,38 @@ def run_benchmark(dir, numRuns):
     #setup.setup()
 
     with TrueCrypt(dir) as fs:
-        truecrypt = Bonnie(name="TrueCrypt", dir=fs.mount_dir).run(numRuns, create_test_size=16)
+        truecrypt = Bonnie(name="TrueCrypt", dir=fs.mount_dir).run(numRuns)
     print("TrueCrypt Output: %s" % truecrypt)
     with TrueCrypt(dir) as fs:
-        truecrypt_1m = Bonnie(name="TrueCrypt-1M", dir=fs.mount_dir).run(numRuns, create_test_size=16, readwrite_test_size="32g:1024k")
+        truecrypt_1m = Bonnie(name="TrueCrypt-1M", dir=fs.mount_dir).run(numRuns, readwrite_test_size="32g:1024k")
     print("TrueCrypt-1M Output: %s" % truecrypt_1m)
 
     with VeraCrypt(dir) as fs:
-        veracrypt = Bonnie(name="VeraCrypt", dir=fs.mount_dir).run(numRuns, create_test_size=16)
+        veracrypt = Bonnie(name="VeraCrypt", dir=fs.mount_dir).run(numRuns)
     print("VeraCrypt Output: %s" % veracrypt)
     with VeraCrypt(dir) as fs:
-        veracrypt_1m = Bonnie(name="VeraCrypt-1M", dir=fs.mount_dir).run(numRuns, create_test_size=16, readwrite_test_size="32g:1024k")
+        veracrypt_1m = Bonnie(name="VeraCrypt-1M", dir=fs.mount_dir).run(numRuns, readwrite_test_size="32g:1024k")
     print("VeraCrypt-1M Output: %s" % veracrypt_1m)
 
     with PlainFs(dir) as fs:
-        plain = Bonnie(name="plain", dir=fs.mount_dir).run(numRuns, create_test_size=64)
+        plain = Bonnie(name="plain", dir=fs.mount_dir).run(numRuns, create_test_size=256)
     print("Plain Output: %s" % plain)
     with PlainFs(dir) as fs:
-        plain_1m = Bonnie(name="plain-1M", dir=fs.mount_dir).run(numRuns, create_test_size=64, readwrite_test_size="32g:1024k")
+        plain_1m = Bonnie(name="plain-1M", dir=fs.mount_dir).run(numRuns, create_test_size=256, readwrite_test_size="32g:1024k")
     print("Plain-1M Output: %s" % plain_1m)
 
     with EncFs(dir) as fs:
-        encfs = Bonnie(name="EncFS", dir=fs.mount_dir).run(numRuns, create_test_size=16)
+        encfs = Bonnie(name="EncFS", dir=fs.mount_dir).run(numRuns)
     print("EncFS Output: %s" % encfs)
     with EncFs(dir) as fs:
-        encfs_1m = Bonnie(name="EncFS-1M", dir=fs.mount_dir).run(numRuns, create_test_size=16, readwrite_test_size="32g:1024k")
+        encfs_1m = Bonnie(name="EncFS-1M", dir=fs.mount_dir).run(numRuns, readwrite_test_size="32g:1024k")
     print("EncFS-1M Output: %s" % encfs_1m)
 
     with CryFs(dir) as fs:
-        cryfs = Bonnie(name="CryFS", dir=fs.mount_dir).run(numRuns, create_test_size=16)
+        cryfs = Bonnie(name="CryFS", dir=fs.mount_dir).run(numRuns)
     print("CryFS Output: %s" % cryfs)
     with CryFs(dir) as fs:
-        cryfs_1m = Bonnie(name="CryFS-1M", dir=fs.mount_dir).run(numRuns, create_test_size=16, readwrite_test_size="32g:1024k")
+        cryfs_1m = Bonnie(name="CryFS-1M", dir=fs.mount_dir).run(numRuns, readwrite_test_size="32g:1024k")
     print("CryFS-1M Output: %s" % cryfs_1m)
 
     output = "\n".join([plain, plain_1m, encfs, encfs_1m, truecrypt, truecrypt_1m, veracrypt, veracrypt_1m, cryfs, cryfs_1m])
